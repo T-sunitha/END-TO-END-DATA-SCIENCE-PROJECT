e36f0d9a029fddf89184eb4deadc155d862ed356
T-sunitha <tummapudisunitha2004@gmail.com>
2025-07-08 19:17:09 +0530

    Update iris_project.py

--- iris_project.py
+++ iris_project.py
@@ -55,3 +55,14 @@ def predict():
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)      
+
+
+output:
+
+Model Accuracy: 1.00
+ * Serving Flask app '__main__'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+ * Restarting with stat
